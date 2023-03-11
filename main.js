356877acb7b7ba17b2cca9a8ddaaf70bc1da3863
oncepk <plustor9.ss@gmail.com>
2023-03-11 16:31:27 +0700

    submit WEB-JS2-Practice

---
+++ main.js
@@ -0,0 +1,22 @@
+let birthYear = 1999;
+let futureYear = 2060;
+let age = futureYear - birthYear;
+
+console.log(
+  `If you were born in ${birthYear}, then in ${futureYear}, you'll be either ${age} or ${
+    age + 1
+  } years old.`
+);
+
+//
+//
+//
+//
+
+let names = "WAN";
+let location = "(Bangkok, Thailand)";
+let hobby = "nothing";
+
+console.log(
+  `Hi, my name is ${names}. I live in ${location} and enjoy ${hobby}.`
+);
